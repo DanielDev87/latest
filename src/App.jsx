d6d0ae3ccc077a888b3cb49b619ee939dad5fdaf
DanielDev87 <danielhymtj@icloud.com>
2026-04-28 20:37:55 -0500

    feat: inicializa proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+  <div className="app">
+      <h1>Taller DevOps - Integración de Componentes</h1>
+      <div className="componentes-container">
+      </div>
+  </div>
+  )
+}
+
+export default App
